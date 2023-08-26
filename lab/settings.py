9c88bae6af98b141a022f8e091dca74d2b55bd9b
Ken Nakamine <kennakamine@gmail.com>
2023-08-27 00:11:05 +0900

    create requirements.txt, use sqlite

--- lab/settings.py
+++ lab/settings.py
@@ -87,15 +87,14 @@ WSGI_APPLICATION = 'lab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -106,6 +105,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
 # Password validation
